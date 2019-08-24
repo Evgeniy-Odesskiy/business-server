2a25159970bac93f9dc48d156f552d18cce1260e
Nikita Samoylov <nikit.samoylov@gmail.com>
2019-08-24 19:51:28 +0300

    add regions table

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const path    = require('path');
+const path = require('path');
 
 // Constants
 const PORT = 9999;
@@ -9,20 +9,27 @@ const HOST = '0.0.0.0';
 
 // Serve the static files from the React app
 
+const regions = require('./data/regions')
+
 // App
 const app = express();
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get('/', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.get('/idi', (req, res) => {
-  res.send('Idi naxyu\n');
+app.get('/regions', (req, res) => {
+    res.send(Object.keys(regions))
 });
-  
+
+app.get('/cities/:id', (req, res) => {
+  var region = Object.keys(regions)[req.params.id]
+  res.send(regions[region])
+});
+
 app.listen(PORT, HOST);
 
 console.log('>>', __dirname);
-console.log('>>',process.env.FOO);
+console.log('>>', process.env.FOO);
 console.log(`Running on http://${HOST}:${PORT}`);
